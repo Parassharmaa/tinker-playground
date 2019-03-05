60ea0b95d4ffeee8242cfca287769075c456d3c0
paraz <mail2paras.s@gmail.com>
2019-03-05 12:39:13 +0530

    [ADD] build

---
+++ build/precache-manifest.94f30d48b1febd0cd49db69e6701b148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cdc1a47a1325f2c9babe",
+    "url": "/static/js/main.cdc1a47a.chunk.js"
+  },
+  {
+    "revision": "cb8e3e685eac82eb5245",
+    "url": "/static/js/2.cb8e3e68.chunk.js"
+  },
+  {
+    "revision": "cdc1a47a1325f2c9babe",
+    "url": "/static/css/main.ad0f5abc.chunk.css"
+  },
+  {
+    "revision": "5ab7ffacf2b15bc7526311636b4c1584",
+    "url": "/index.html"
+  }
+];
